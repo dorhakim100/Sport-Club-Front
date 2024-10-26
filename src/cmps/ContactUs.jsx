99ed651f6ef03adc705593ea7d899a8960c528d9
Dor <dorhakim100@gmail.com>
2024-10-26 11:54:45 +0300

    Add send message

--- src/cmps/ContactUs.jsx
+++ src/cmps/ContactUs.jsx
@@ -1,12 +1,47 @@
+import { useState, useEffect } from 'react'
+import { useNavigate, useSearchParams, useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+
+import { messageService } from '../services/message/message.service'
+import { addMessage } from '../store/actions/message.actions'
 
 import { HeadContainer } from './HeadContainer'
 
 import { Button } from '@mui/material'
 import { LoadingButton } from '@mui/lab'
+import { Message } from '@mui/icons-material'
+import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
+import { setIsLoading } from '../store/actions/system.actions'
 
 export function ContactUs() {
   const prefs = useSelector((stateSelector) => stateSelector.systemModule.prefs)
+
+  const [editMessage, setEditMessage] = useState(
+    messageService.getEmptyMessage()
+  )
+
+  function handleChange({ target }) {
+    const field = target.name
+    const value = target.value
+
+    setEditMessage({ ...editMessage, [field]: value })
+  }
+
+  async function onSend() {
+    console.log(editMessage)
+    delete editMessage._id
+    try {
+      setIsLoading(true)
+      const addedMessage = await addMessage(editMessage)
+      showSuccessMsg(prefs.isEnglish ? 'Message sent' : 'הודעה נשלחה')
+      setEditMessage(messageService.getEmptyMessage())
+    } catch (err) {
+      console.log(err)
+      showErrorMsg(prefs.isEnglish ? `Could't send message` : 'הודעה לא נשלחה')
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   return (
     <div className='contact-us-container'>
@@ -23,19 +58,29 @@ export function ContactUs() {
       >
         <input
           type='text'
-          name=''
+          name='title'
           id=''
           placeholder={prefs.isEnglish ? 'Name' : 'שם'}
+          onChange={handleChange}
+          value={editMessage.title}
         />
         <input
           type='text'
-          name=''
+          name='phone'
           id=''
           placeholder={prefs.isEnglish ? 'Phone' : 'טלפון'}
+          onChange={handleChange}
+          value={editMessage.phone}
         />
-        <textarea name='' id='' style={{ resize: 'none' }} />
+        <textarea
+          name='content'
+          id=''
+          style={{ resize: 'none' }}
+          onChange={handleChange}
+          value={editMessage.content}
+        />
       </div>{' '}
-      <LoadingButton variant='contained'>
+      <LoadingButton variant='contained' onClick={() => onSend()}>
         {prefs.isEnglish ? 'Send' : 'שליחה'}
       </LoadingButton>
     </div>
--- src/services/message/message.service.js
+++ src/services/message/message.service.js
@@ -1,3 +1,4 @@
+import { storageService } from '../async-storage.service'
 import { makeId } from '../util.service'
 
 const STORAGE_KEY = 'message'
