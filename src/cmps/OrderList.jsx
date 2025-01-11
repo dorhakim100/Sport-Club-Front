d2004386ef19efe33a93d1f8627becca72a1705d
Dor <dorhakim100@gmail.com>
2025-01-11 14:28:45 +0200

    Add Controller and sort button to OrderList

--- src/cmps/OrderList.jsx
+++ src/cmps/OrderList.jsx
@@ -11,17 +11,35 @@ import { showErrorMsg } from '../services/event-bus.service.js'
 import { setIsLoading } from '../store/actions/system.actions.js'
 import { paymentService } from '../services/payment/payment.service.js'
 import { loadPayments } from '../store/actions/payment.actions.js'
+import { Controller } from './Controller.jsx'
 
 import Divider from '@mui/material/Divider'
+import { Button } from '@mui/material'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
 
-export function OrderList({ user, orders }) {
+export function OrderList({ user, orders, maxPage, filter, setFilterBy }) {
   const prefs = useSelector((stateSelector) => stateSelector.systemModule.prefs)
   const text = {
     he: 'הזמנות',
     eng: 'Orders',
   }
 
-  useEffect(() => {}, [user])
+  const setSort = () => {
+    setFilterBy({ ...filter, sortDir: filter.sortDir * -1 })
+  }
+
+  const sortText = () => {
+    const text = {}
+    if (filter.sortDir === 1) {
+      text.he = `סדר יורד`
+      text.eng = `Descending Order`
+    } else {
+      text.he = `סדר עולה`
+      text.eng = 'Ascending Order'
+    }
+    return text
+  }
 
   return (
     <div className='orders-list-container'>
@@ -29,21 +47,22 @@ export function OrderList({ user, orders }) {
       <b className='hello-text'>
         {prefs.isEnglish ? `Hello, ${user.fullname}` : `שלום, ${user.fullname}`}
       </b>
+      <div className='controller-container'>
+        <Controller filter={filter} maxPage={maxPage} setFilter={setFilterBy} />
+        <Button variant='contained' onClick={setSort}>
+          {prefs.isEnglish ? sortText().eng : sortText().he}
+          {filter.sortDir === 1 ? (
+            <KeyboardArrowDownIcon />
+          ) : (
+            <KeyboardArrowUpIcon />
+          )}
+        </Button>
+      </div>
       <div className='list-container'>
         {user.ordersIds.length > 0 ? (
           orders.map((order, index) => {
             return (
-              <>
-                <OrderPreview order={order} key={`${order._id}${makeId()}`} />
-
-                {/* {order.length > 1 && index !== orders.length - 1 && (
-                <Divider
-                  orientation='horizontal'
-                  flexItem
-                  key={`${order._id}Divider`}
-                />
-                  )} */}
-              </>
+              <OrderPreview order={order} key={`${order._id}${makeId()}`} />
             )
           })
         ) : (
