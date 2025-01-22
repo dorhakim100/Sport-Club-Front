e5cdc03c2dfa92c9476c0c666ee4a3450381aa63
Dor <dorhakim100@gmail.com>
2025-01-22 14:43:40 +0200

    Add dark mode loader

--- src/cmps/Loader.jsx
+++ src/cmps/Loader.jsx
@@ -1,17 +1,29 @@
 import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
 
 import loaderSvg from '../../public/imgs/swimming.gif' // Adjust the path according to your project structure
+import loaderSvgDarkMode from '../../public/imgs/swimming-dark-mode.gif'
 
 export function Loader() {
   const prefs = useSelector((storeState) => storeState.systemModule.prefs)
   const isLoading = useSelector(
     (storeState) => storeState.systemModule.isLoading
   )
+
+  const [loader, setLoader] = useState(loaderSvg)
+
+  useEffect(() => {
+    if (prefs.isDarkMode) {
+      setLoader(loaderSvgDarkMode)
+    } else {
+      setLoader(loaderSvg)
+    }
+  }, [prefs.isDarkMode])
   return (
     isLoading && (
       <div className='loader-container'>
         <img
-          src={loaderSvg}
+          src={loader}
           alt=''
           style={{
             transform: prefs.isEnglish ? '' : 'scaleX(-1)',
