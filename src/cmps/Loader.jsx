ae24934f22defd54d198d22271970f988790691e
Dor <dorhakim100@gmail.com>
2025-02-25 20:28:23 +0200

    Add better Lottie loader animation

--- src/cmps/Loader.jsx
+++ src/cmps/Loader.jsx
@@ -1,35 +1,48 @@
 import { useSelector } from 'react-redux'
 import React, { useEffect, useState } from 'react'
+import Lottie from 'react-lottie'
 
 import loaderSvg from '../../public/imgs/swimming.gif' // Adjust the path according to your project structure
 import loaderSvgDarkMode from '../../public/imgs/swimming-dark-mode.gif'
+
+import loaderJson from '../../public/imgs/loader-jsons/loader.json'
+import loaderDarkJson from '../../public/imgs/loader-jsons/loader_dark_mode.json'
+import { setIsLoading } from '../store/actions/system.actions'
 
 export function Loader() {
   const prefs = useSelector((storeState) => storeState.systemModule.prefs)
   const isLoading = useSelector(
     (storeState) => storeState.systemModule.isLoading
   )
+  const [loader, setLoader] = useState(loaderJson)
 
-  const [loader, setLoader] = useState(loaderSvg)
+  const defaultOptions = {
+    loop: false,
+    autoplay: true, // Animation will autoplay
+    animationData: loader,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  }
 
   useEffect(() => {
     if (prefs.isDarkMode) {
-      setLoader(loaderSvgDarkMode)
+      setLoader(loaderDarkJson)
     } else {
-      setLoader(loaderSvg)
+      setLoader(loaderJson)
     }
   }, [prefs.isDarkMode])
+
   return (
     isLoading && (
-      <div className='loader-container'>
-        <img
-          src={loader}
-          alt=''
-          style={{
-            transform: prefs.isEnglish ? '' : 'scaleX(-1)',
-            display: 'block',
-          }}
-        />
+      <div
+        className='loader-container'
+        style={{
+          transform: prefs.isEnglish ? '' : 'scaleX(-1)',
+          display: 'block',
+        }}
+      >
+        <Lottie options={defaultOptions} width={'100%'} height={'100%'} />
       </div>
     )
   )
